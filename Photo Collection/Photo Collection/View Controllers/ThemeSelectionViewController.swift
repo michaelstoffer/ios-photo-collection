c20c9bc5e63a62c5c4e8341f09f955ee30d17338
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-05-16 12:49:17 -0400

    Completed ThemeSelectionViewController Scene

---
+++ Photo Collection/Photo Collection/View Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ThemeSelectionViewController.swift
+//  Photo Collection
+//
+//  Created by Michael Stoffer on 5/16/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selectDarkTheme(_ sender: UIButton) {
+    }
+
+    @IBAction func selectBlueTheme(_ sender: UIButton) {
+    }
+}
